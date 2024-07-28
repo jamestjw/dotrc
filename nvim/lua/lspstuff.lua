6358b48ce60c98ab103a307d268ab946cae6c42d
James Tan <james_tan97@outlook.com>
2024-07-27 23:03:59 -0400

    (nvim) add `oil.nvim` plugin

--- nvim/lua/lspstuff.lua
+++ nvim/lua/lspstuff.lua
@@ -38,20 +38,20 @@ local on_attach = function(_, buffer)
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = buffer }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set("n", "<space>wl", function()
+	map("n", "gD", vim.lsp.buf.declaration, bufopts)
+	map("n", "gd", vim.lsp.buf.definition, bufopts)
+	map("n", "K", vim.lsp.buf.hover, bufopts)
+	map("n", "gi", vim.lsp.buf.implementation, bufopts)
+	map("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	map("n", "<space>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, bufopts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	map("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	map("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+	map("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	map("n", "gr", vim.lsp.buf.references, bufopts)
 end
 
 local lsp_flags = {
