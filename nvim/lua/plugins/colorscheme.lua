19e3a50584385013d1dd8cec01e38f9e3f7ed18c
James Tan <james_tan97@outlook.com>
2024-08-26 10:58:37 -0400

    (nvim) change colorscheme to onedark

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,27 @@
 -- return {
-	-- "catppuccin/nvim",
-	-- name = "catppuccin",
-	-- priority = 1000,
-	-- opts = function()
-		-- vim.cmd.colorscheme("catppuccin-mocha")
-	-- end,
+-- "catppuccin/nvim",
+-- name = "catppuccin",
+-- priority = 1000,
+-- opts = function()
+-- vim.cmd.colorscheme("catppuccin-mocha")
+-- end,
+-- }
+
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   priority = 1000,
+--   opts = function()
+--     vim.cmd.colorscheme("kanagawa")
+--   end
 -- }
 
 return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  opts = function()
-    vim.cmd.colorscheme("kanagawa")
-  end
+	"navarasu/onedark.nvim",
+	priority = 1000,
+	config = function()
+		require("onedark").setup({
+			style = "warmer",
+		})
+		require("onedark").load()
+	end,
 }
