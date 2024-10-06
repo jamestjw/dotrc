157b054954135bd7d1cdfa07d1a20d45c103a2d9
James Tan <james_tan97@outlook.com>
2024-10-06 13:02:51 -0400

    (nvim) update autopair config for ocaml

--- nvim/lua/config/autopairs.lua
+++ nvim/lua/config/autopairs.lua
@@ -8,8 +8,8 @@ function M.setup()
 		enable_check_bracket_line = false,
 	})
 
-	-- remove add single quote on filetype scheme or lisp
-	npairs.get_rule("'")[1].not_filetypes = { "scheme", "lisp" }
+	-- Remove automatic single quotes in Scheme, Lisp and OCaml files.
+	npairs.get_rule("'")[1].not_filetypes = { "scheme", "lisp", "ocaml" }
 	npairs.get_rule("'")[1]:with_pair(cond.not_after_text("["))
 end
 
--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -27,7 +27,7 @@ cmp.setup({
 		-- is no vim docs, but you can't have select = true here _unless_ you are
 		-- also using the snippet stuff. So keep in mind that if you remove
 		-- snippets you need to remove this select
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
 		-- I use tabs... some say you should stick to ins-completion but this is
 		-- just here as an example
