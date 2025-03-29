e435b6b9a446a0b727a46bd6240d5cb0d0f383e5
James Tan <james_tan97@outlook.com>
2025-03-29 12:17:53 -0400

    (i3) some changes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 # TODO: Setup symlinks
+# symlink git, bash, nvim, i3 stuff
 
 sudo dnf install xclip
+
+# TODO: neovim
 
 # TODO: Setup python
 # TODO: Setup rust
@@ -10,3 +13,10 @@ sudo dnf install xclip
 # TODO: Setup tmuxinator (and tmux)
 
 # TODO: setup ondir
+
+# TODO: install tools
+#   btop
+
+# Lazygit
+sudo dnf copr enable atim/lazygit -y
+sudo dnf install lazygit -y
