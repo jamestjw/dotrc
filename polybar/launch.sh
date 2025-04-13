aca142baa8b3be4adddb954444206f5dcdfa167c
James Tan <james_tan97@outlook.com>
2025-04-13 16:20:10 -0400

    (i3) change polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,10 +8,10 @@ polybar-msg cmd quit
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload i3_bar &
   done
 else
-  polybar --reload example &
+  polybar --reload i3_bar &
 fi
 
 echo "Bars launched..."
