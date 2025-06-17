38ded9b1a896b5a238568c4b074c84884e6b9015
James Tan <james_tan97@outlook.com>
2025-06-17 10:43:46 -0400

    (nvim) fix treesitter config?

--- nvim/lua/plugins/nvim-ts-context-commentstring.lua
+++ nvim/lua/plugins/nvim-ts-context-commentstring.lua
@@ -3,7 +3,11 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {
-    enable_autocmd = false,
-  },
+
+  config = function()
+    require("ts_context_commentstring").setup({
+      enable_autocmd = false,
+    })
+    vim.g.skip_ts_context_commentstring_module = true
+  end,
 }
