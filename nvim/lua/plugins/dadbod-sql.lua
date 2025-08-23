9e14ef7d648fea9fb2fbe8f76a53ff2fb12eb595
James Tan <james_tan97@outlook.com>
2025-08-23 01:22:05 -0400

    (nvim) update dadbod config

--- nvim/lua/plugins/dadbod-sql.lua
+++ nvim/lua/plugins/dadbod-sql.lua
@@ -13,5 +13,7 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_execute_on_save = 0
+    vim.g.db_ui_auto_execute_table_helpers = 0
   end,
 }
